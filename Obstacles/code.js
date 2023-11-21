3c649b196c1031805145223936c94eba00bac32f
isabellesundby <144380713+isabellesundby@users.noreply.github.com>
2023-11-21 13:12:23 -0600

    Add files via upload

--- Obstacles/code.js
+++ Obstacles/code.js
@@ -3,6 +3,8 @@ let w = 400
 let h = 400
 dx = 10
 let rects = [[50,100,50], [150,200,60],[300,50,20]]
+let min = 0
+let max = 80
 
 setInterval(drawFrame, 200)
 
@@ -16,7 +18,11 @@ function updateData(){
     for (let i = 0; i < rects.length; ++i){
         rects[i][0] -= dx
         if (rects[i][0] < 0){
-            rects[i][0] = w
+            rects[i][0] = w + Math.floor(Math.random()*(max-min)+min)
+            rects[i][0] += Math.floor(Math.random()*(max-min)+min)
+            if (rects[i][1] <= 0 || rects[i][1] >= 3/4*h){
+                rects[i][1] = 1/4*h
+            }
         }
     }
 }
@@ -30,7 +36,7 @@ function drawBackground(){
 
 function drawRects(){
     for (let i=0; i < rects.length; ++i){
-        brush.fillStyle = "#FF00000"
+        brush.fillStyle = "#FF0000"
         brush.fillRect (rects[i][0], rects[i][1], rects[i][2], 3/4*h-rects[i][1])
     }
 }
